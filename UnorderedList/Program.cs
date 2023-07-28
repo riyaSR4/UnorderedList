1b17e384e680c223ee49aeab64cb1bdc97cbf82e
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-28 15:44:50 +0530

    [Riya] Add : Unordered List

--- UnorderedList/Program.cs
+++ UnorderedList/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Net.NetworkInformation;
+using UnorderedList.UnorderedList;
+
+namespace UnorderedList
+{
+    class Program
+    {
+        static string unOrderedFilePath = @"D:\BridgeLabs Training\UnorderedList\UnorderedList\UnorderedList\UnOrderList.txt";
+        static void Main(string[] args)
+        {
+            Operation operation = new Operation();
+            operation.ReadFileAndPerformOperation(unOrderedFilePath);
+        }
+    }
+}
---
+++ UnorderedList/UnorderedList/Operation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnorderedList.UnorderedList
+{
+    public class Operation
+    {
+        public void ReadFileAndPerformOperation(string filePath)
+        {
+            LinkedList<string> linkedList = new LinkedList<string>();
+            string readAllText = File.ReadAllText(filePath);
+            string[] words = readAllText.Split(" ");
+            foreach(var data in words)
+            {
+                linkedList.Add(data);
+            }
+            linkedList.Display();
+            Console.WriteLine("\nEnter a input :");
+            string input = Console.ReadLine();
+            linkedList.Operation(input);
+            linkedList.Display();
+        }
+    }
+}
